ee0f67ff42e91b74dfdd0116988ea62867357ac7
qingfengwuyou <h3658686@126.com>
2018-05-12 16:32:31 +0800

    AppModule 初始化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,32 @@
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID, APP_INITIALIZER } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { UEditorModule } from 'ngx-ueditor';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { StartupService } from '@core/startup/startup.service';
+import { DelonModule } from './delon.module';
+import { CoreModule } from '@core/core.module';
+import { SharedModule } from '@shared/shared.module';
+import { NgxTinymceModule } from 'ngx-tinymce';
+import { JsonSchemaModule } from '@shared/json-schema/json-schema.module';
+import { SimpleInterceptor } from '@delon/auth';
+import { DefaultInterceptor } from '@core/net/default.interceptor';
+import { ALAIN_I18N_TOKEN } from '@delon/theme';
+import { I18NService } from '@core/i18n/i18n.service';
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, `assets/i18n/`, '.json');
+}
 
+export function StartupServiceFactory(
+  startupService: StartupService,
+): Function {
+  return () => startupService.load();
+}
 @NgModule({
   declarations: [
     AppComponent
@@ -16,8 +35,23 @@ import { UEditorModule } from 'ngx-ueditor';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    AppRoutingModule, // 暂时用这个
+    DelonModule.forRoot(),
+    CoreModule,
+    SharedModule,
 
+
+    AppRoutingModule, // 暂时用这个
+    // LayoutModule,  //先不处理
+    // RoutesModule,  //先不处理
+
+    // i18n
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
     // thirds
     UEditorModule.forRoot({
       // 指定ueditor.js路径目录
@@ -27,20 +61,24 @@ import { UEditorModule } from 'ngx-ueditor';
         themePath: '/assets/ueditor/themes/'
       }
     }),
+    NgxTinymceModule.forRoot({
+      baseURL: '//cdn.bootcss.com/tinymce/4.7.4/',
+    }),
     // JSON-Schema form
+    JsonSchemaModule,
   ],
   providers: [
-    // { provide: LOCALE_ID, useValue: 'zh-Hans' },
-    // { provide: HTTP_INTERCEPTORS, useClass: SimpleInterceptor, multi: true },
-    // { provide: HTTP_INTERCEPTORS, useClass: DefaultInterceptor, multi: true },
-    // { provide: ALAIN_I18N_TOKEN, useClass: I18NService, multi: false },
-    // StartupService,
-    // {
-    //   provide: APP_INITIALIZER,
-    //   useFactory: StartupServiceFactory,
-    //   deps: [StartupService],
-    //   multi: true,
-    // },
+    { provide: LOCALE_ID, useValue: 'zh-Hans' },
+    { provide: HTTP_INTERCEPTORS, useClass: SimpleInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: DefaultInterceptor, multi: true },
+    { provide: ALAIN_I18N_TOKEN, useClass: I18NService, multi: false },
+    StartupService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: StartupServiceFactory,
+      deps: [StartupService],
+      multi: true,
+    },
   ],
   bootstrap: [AppComponent]
 })
